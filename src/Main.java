f53e42cdbaaa10139d9c86c673dc48bf454d493a
it21059940 <87602629+it21059940@users.noreply.github.com>
2021-11-16 23:31:34 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String args[]) {
         String question = "What is the largest planet in our solar system?";
         String choiceOne = "earth";
@@ -10,28 +9,23 @@ public class Main {
 
         String correctAnswer = choiceTwo;
 
-        // Write a print statement asking the question
+        // A print statement asking the question
         System.out.println(question);
 
-        // Write a print statement giving the answer choices
+        // A print statement giving the answer choices
         System.out.println("Choose one of the following: " +
                 choiceOne + ", " + choiceTwo + ", or " + choiceThree + ".");
 
         // Have the user input an answer
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
         // Retrieve the user's input
-        String input = scanner.next();
+        String userAns = input.next();
 
-        // If the user's input matches the correctAnswer...
-        // then the user is correct and we want to print out a congrats message to the user.
-        if(correctAnswer.equals(input.toLowerCase())) {
+        //check if userans is equal to correctans after converting user ans to lower case
+        if(correctAnswer.equals(userAns.toLowerCase())) {
             System.out.println("Congrats! That's the correct answer");
         } else {
             System.out.println("You are incorrect. The correct answer is " + correctAnswer);
         }
-        // If the user's input does not match the correctAnswer...
-        // then the user is incorrect and we want to print out a message saying that the user is incorrect as well as what the correct choice was.
-
-    }
-
+     }
 }
